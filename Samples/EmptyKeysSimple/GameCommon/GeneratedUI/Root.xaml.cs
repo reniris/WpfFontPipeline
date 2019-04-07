879a2543764916503a23501847329b605b5f06ab
reniris <nyoronu@gmail.com>
2019-04-07 20:37:57 +0900

    サンプル追加

---
+++ Samples/EmptyKeysSimple/GameCommon/GeneratedUI/Root.xaml.cs
@@ -0,0 +1,69 @@
+// -----------------------------------------------------------
+//  
+//  This file was generated, please do not modify.
+//  
+// -----------------------------------------------------------
+namespace EmptyKeys.UserInterface.Generated {
+    using System;
+    using System.CodeDom.Compiler;
+    using System.Collections.ObjectModel;
+    using EmptyKeys.UserInterface;
+    using EmptyKeys.UserInterface.Charts;
+    using EmptyKeys.UserInterface.Data;
+    using EmptyKeys.UserInterface.Controls;
+    using EmptyKeys.UserInterface.Controls.Primitives;
+    using EmptyKeys.UserInterface.Input;
+    using EmptyKeys.UserInterface.Interactions.Core;
+    using EmptyKeys.UserInterface.Interactivity;
+    using EmptyKeys.UserInterface.Media;
+    using EmptyKeys.UserInterface.Media.Effects;
+    using EmptyKeys.UserInterface.Media.Animation;
+    using EmptyKeys.UserInterface.Media.Imaging;
+    using EmptyKeys.UserInterface.Shapes;
+    using EmptyKeys.UserInterface.Renderers;
+    using EmptyKeys.UserInterface.Themes;
+    
+    
+    [GeneratedCodeAttribute("Empty Keys UI Generator", "3.1.0.0")]
+    public partial class Root : UIRoot {
+        
+        private Grid e_0;
+        
+        private TextBlock e_1;
+        
+        public Root() : 
+                base() {
+            this.Initialize();
+        }
+        
+        public Root(int width, int height) : 
+                base(width, height) {
+            this.Initialize();
+        }
+        
+        private void Initialize() {
+            Style style = RootStyle.CreateRootStyle();
+            style.TargetType = this.GetType();
+            this.Style = style;
+            this.InitializeComponent();
+        }
+        
+        private void InitializeComponent() {
+            // e_0 element
+            this.e_0 = new Grid();
+            this.Content = this.e_0;
+            this.e_0.Name = "e_0";
+            // e_1 element
+            this.e_1 = new TextBlock();
+            this.e_0.Children.Add(this.e_1);
+            this.e_1.Name = "e_1";
+            this.e_1.HorizontalAlignment = HorizontalAlignment.Center;
+            this.e_1.VerticalAlignment = VerticalAlignment.Center;
+            this.e_1.Text = "こんにちは 世界";
+            this.e_1.FontFamily = new FontFamily("Ume Gothic");
+            this.e_1.FontSize = 20F;
+            this.e_1.FontStyle = FontStyle.Bold;
+            FontManager.Instance.AddFont("Ume Gothic", 20F, FontStyle.Bold, "Ume_Gothic_15_Bold");
+        }
+    }
+}
